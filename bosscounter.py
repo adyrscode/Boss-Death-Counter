6bed5e6dc05f479a4efae624237aaeec8d972cfe
adyr <173387604+adyrscode@users.noreply.github.com>
2025-10-16 19:40:34 +0200

    Added Boss Selection

--- bosscounter.py
+++ bosscounter.py
@@ -20,13 +20,18 @@ if len(bosses) == (0):
 else:
     selected_boss = data["saved_boss"]
 
-print(f"""Current boss selected: {selected_boss}
-Press 1 to add death to {selected_boss}.
-Press 2 to remove death from {selected_boss}
-Press 3 to select a different boss.
-Press 4 to add a new boss.
-Press 5 to display bosses.
-Press Delete to delete {selected_boss}""")
+if selected_boss == None:
+    print_selected_boss = "selected boss"
+else:
+    print_selected_boss = selected_boss
+
+print(f"""Current boss selected: {print_selected_boss}
+Press 1 to add death to {print_selected_boss}.
+Press 2 to remove death from {print_selected_boss}
+Press 3 to add a new boss.
+Press 4 to display bosses.
+Press Ctrl + Shift + Left/Right arrow key to switch selected boss.
+Press Delete to delete {print_selected_boss}""")
 
 def save_file():
     with open(filename, "w") as file: # saves the file
@@ -38,7 +43,7 @@ def plus_death(event): # event_object is passed by keyboard to this function
     else:
         bosses[selected_boss] = bosses[selected_boss] + 1
         save_file()
-        print(f"+1 death to {selected_boss}")
+        print(f"+1 death to {selected_boss}. Total deaths: {bosses[selected_boss]}")
 
 def min_death(event):
     if event.name == ('down'): # safeguard to prevent arrow keys triggering functions
@@ -52,42 +57,73 @@ def min_death(event):
         save_file()
         print(f"-1 death from {selected_boss}")
 
-def switch_boss(event):
-    pass
-
-def add_boss(event):
-    if event.name == ('left'):
+def switch_boss(operator):
+    global selected_boss
+    if selected_boss == None:
+        print("You don't have any bosses to switch to yet.")
         return
-    
-    keyboard.remove_all_hotkeys()
-    keyboard.send('backspace')
-    new_boss = input("Please enter a boss name (press delete to cancel): ")
+    boss_index = 0
+    for boss in bosses:
+        if boss == selected_boss:
+            break
+        else:
+            boss_index += 1
 
-    if new_boss == "" or None:
-        print("Boss not added.")
+    if operator == "+": # don't fucking question this code, it works
+        if boss_index + 1 == len(bosses):
+            selected_boss = list(bosses)[0]
+        else:
+            selected_boss = list(bosses)[boss_index + 1]
+
+    elif operator == "-":
+        if boss_index - 1 < 0:
+            selected_boss = list(bosses)[len(bosses) - 1]
+        else:
+            selected_boss = list(bosses)[boss_index - 1]
+
+    print(f"Selected {selected_boss}. Deaths: {bosses[selected_boss]}")
+
+def add_boss(event):    
+    global listening
+    listening = False
+    keyboard.send('backspace')
+    new_boss = input(f"Please enter a boss name (enter {"cancel"} to cancel): ")
+
+    if new_boss == "" or new_boss == "cancel":
         listening = True
+        print("Boss not added.")
         return
     
     elif new_boss in bosses:
-        print("Boss already exists.")
         listening = True
+        print("Boss already exists.")
         return
     
     else:
+        listening = True
         bosses[new_boss] = 0
         save_file()
         global selected_boss
         selected_boss = new_boss
-        print(selected_boss, "added and selected.")
-        listening = True
+        print(new_boss, "added and selected.")
         return
 
 def display_boss(event):
+    if event.name == ('left'): # arrow keys trigger 2, 4, 6 and 8 as well.
+            return
+    
+    if len(bosses) == 0:
+        print("You don't have any bosses added yet.")
+        return
+    
     for boss in bosses:
         print(f"{boss}{":"} {bosses[boss]} deaths.")
 
 def delete_boss(event):
     global selected_boss
+    if selected_boss == None:
+        print("Please select a boss to delete.")
+        return
     confirmation = input(f"Are you sure you want to delete {selected_boss}? Type {"delete"} to confirm. ")
 
     if confirmation != "delete":
@@ -101,11 +137,14 @@ def delete_boss(event):
         save_file()
         selected_boss = None
 
+# while listening:
 keyboard.on_press_key('1', plus_death)
 keyboard.on_press_key('2', min_death)
-keyboard.on_press_key('3', switch_boss)
-keyboard.on_press_key('4', add_boss)
-keyboard.on_press_key('5', display_boss)
+keyboard.add_hotkey('ctrl+shift+left', switch_boss, args=["-"])
+keyboard.add_hotkey('ctrl+shift+right', switch_boss, args=["+"])
+# keyboard.on_press_key('3', switch_boss)
+keyboard.on_press_key('3', add_boss)
+keyboard.on_press_key('4', display_boss)
 keyboard.on_press_key('delete', delete_boss)
 
 def exit():
